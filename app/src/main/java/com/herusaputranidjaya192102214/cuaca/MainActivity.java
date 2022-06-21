338160a1f812bac2f19d419ddc7529bb1049e816
HeruNidjaya <herusaputranidjaya01@gmail.com>
2022-06-22 01:44:04 +0700

    Initial commit

---
+++ app/src/main/java/com/herusaputranidjaya192102214/cuaca/MainActivity.java
@@ -0,0 +1,14 @@
+package com.herusaputranidjaya192102214.cuaca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
